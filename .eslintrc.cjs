b770f72479e948e55152740c738cb7f305b4302c
王芳宇 <1206001352@qq.com>
2024-04-08 16:38:41 +0800

    V1：登录与简单图表

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting"
+  ],
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
+        "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
+        "cypress/support/**/*.{js,ts,jsx,tsx}"
+      ],
+      extends: ["plugin:cypress/recommended"]
+    },
+    {
+      files: ["*.cjs"],
+      env: {
+        node: true
+      },
+      rules: {
+        // 在node环境需要require
+        "@typescript-eslint/no-var-requires": 0
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest"
+  }
+}
